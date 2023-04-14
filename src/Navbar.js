919c5a478f21bff4d3c71202f282be57a3aba765
yakubu-josiah <jossysmothest007@gmail.com>
2023-04-14 23:51:31 +0100

    using inline styles on elements

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Welcome to my Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/" style={{
+                    color: "#fff",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "5px",
+                    fontSize: "13px"
+                }}>Home</a>
                 <a href="/create">Add New Post</a>
                 <a href="/show">All Blog Posts</a>
                 <a href="/more">More</a>
