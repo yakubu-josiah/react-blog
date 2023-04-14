a7f4770c3e3acd48a1f53bebf91a04fa060b610e
yakubu-josiah <jossysmothest007@gmail.com>
2023-04-14 22:38:36 +0100

    adding styles the pages using the index.css file

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,13 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Welcome to my Blog</h1>
-            <a href="/">Home</a>
-            <a href="/create">Add New Post</a>
-            <a href="/show">List of Post</a>
-            <a href="/more">More</a>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Add New Post</a>
+                <a href="/show">All Blog Posts</a>
+                <a href="/more">More</a>
+            </div>
+            
         </nav>
         
     );
