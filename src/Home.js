143e918ed39dc2fe1cdc0655bd99c801c3961c09
yakubu-josiah <jossysmothest007@gmail.com>
2023-04-15 02:46:38 +0100

    Adding click events to components & event objects

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        alert('hello world', e.getModifierState);
+    }
+    const paramClick = (param, e) => {
+        console.log('Hey ' + param + '! ermmm... I really dont remember your Lastname', e);
+    }
+     
     return ( 
         <div className="home">
             <h1>All Homepage content goes in here</h1>
+            <button onClick={handleClick}>Hello World Button</button>
+            <button onClick={(e) => paramClick('Josiah', e.target)}>Do you know me?</button>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
             <div className="links">
                 <a href="/" style={{
                     color: "#fff",
-                    backgroundColor: "#f1356d",
+                    backgroundColor: "#6b2bde",
                     borderRadius: "5px",
                     fontSize: "13px"
                 }}>Home</a>
-                <a href="/create">Add New Post</a>
+                <a href="/create">Add New Blog</a>
                 <a href="/show">All Blog Posts</a>
                 <a href="/more">More</a>
             </div>
