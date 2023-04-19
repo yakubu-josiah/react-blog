168f2bca13db21b193d45b98b8981aba418a9d89
yakubu-josiah <jossysmothest007@gmail.com>
2023-04-19 01:32:26 +0100

    mapping through with hooks n displaying all blogs

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="content">
-        {/* <Home /> */}
-      </div>
+      {/* <div className="content">
+        <Home />
+      </div> */}
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -6,35 +6,31 @@ const Home = () => {
     const [name, setName] = useState('Hey you! you skipped telling me your name ');
     const [age, setAge] = useState('your age. Click the button below to fix this.');
     const [button, setButton] = useState('Fix? Click here')
-    const quest = () => {
+    function quest() {
         let theName = prompt('What\'s your name please?');
         let theAge = prompt('Now tell me your Age');
-        
+
         setName('Good, welcome here ' + theName);
-        setAge('it is good to know that you\'re ' +theAge+ 'years old.');
+        setAge('it is good to know that you\'re ' + theAge + 'years old.' || ' ');
         setButton('Wrong? Update here');
     }
     
-    //target function
-    const handleClick = (e) => {
-        let name = prompt('What\'s your name?');
-        alert('Welcome here ' + name + '! ermmm... I didn\'t request for your lastname ' +name+ '.');
-    }
-    const paramClick = (param, e) => {
-        console.log('Hey ' + param + '! ermmm... I really dont remember your Lastname', e);
-    }
-    
+    const [blog, setBlog] = useState([
+        { title: 'First Blog Post', author: 'Micheal Faraday', id: 1, body: 'lorem ipsum.....' },
+        { title: 'Second Blog Post', author: 'Charles Babbage', id: 2, body: 'lorem ipsum.....' },
+        { title: 'Third Blog Post', author: 'Issac Newton', id: 3, body: 'lorem ipsum.....' },
 
-    
+    ]);
     return ( 
         <div className="mtb">
-            <h1>All Homepage content goes in here</h1>
-            <button onClick={handleClick}>Hey You!</button>
-            <button onClick={(e) => paramClick('Josiah', e.target)}>Click and Check Console</button>
-            <div className="mtb">
-                <p>{name} and {age}</p>
-                <button onClick={quest}> {button} </button>
-            </div>
+            <p>{name} and {age}</p>
+            <button onClick={quest}> {button} </button>
+            {blog.map((post) => (
+                <div className="preview-list" key={post.id}>
+                    <h2> {post.title} </h2>
+                    <p> This was written by: <span>{ post.author }</span></p>
+                </div>
+            ))}        
         </div>
     );
 }
