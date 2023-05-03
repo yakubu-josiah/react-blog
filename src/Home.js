d26f98886e99b350e3b6b6eff0fb1d7f76cbc2d4
yakubu-josiah <jossysmothest007@gmail.com>
2023-05-03 01:38:04 +0100

    setting error handlers if server status is 400+

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,9 @@ const Home = () => {
     //useState function
     const [name, setName] = useState('Hey you! you skipped telling me your name ');
     const [age, setAge] = useState('your age. Click the button below to fix this.');
-    const [button, setButton] = useState('Fix? Click here')
+    const [button, setButton] = useState('Fix? Click here');
+    const [isLoading, setIsLoading] = useState(true);
+    const [bibErr, setBibErr] = useState(null);
     function quest() {
         let theName = prompt('What\'s your name please?');
         let theAge = prompt('Now tell me your Age');
@@ -23,15 +25,29 @@ const Home = () => {
         alert('You have successfully deleted ' + oldTitle.writer + '\'s writings. ');
         const newBlog = blog.filter(post => post.chapter !== id);
         setBlog(newBlog);
+        setIsLoading(false);
     } 
 
 
     useEffect(() => {
-      fetch('http://localhost:3200/bibleVerses')
-        .then(res => res.json())
+      fetch('http://localhost:3210/bibleVerses')
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Couldn\'t fetch data! Please refresh page server is down...');
+            }
+            return res.json();
+        })
         .then(data => {
-            setBlog(data);
-        });
+            setInterval(() => {
+                setBlog(data);
+                setBibErr(null);
+                setIsLoading(false);
+            }, 1000);
+        })
+        .catch(err => {
+            setBibErr(err.message);
+            setIsLoading(false);
+        })
     }, [])
     
 
@@ -39,6 +55,20 @@ const Home = () => {
         <div className="mtb">
             <p>{name} and {age}</p>
             <button onClick={quest}> {button} </button>
+            { bibErr && 
+                <div className="loader">
+                    <div className="aniMe"><p>{ bibErr }</p></div>
+                </div>
+                
+            }
+            { isLoading && 
+                <div className="loader">
+                    <div className="aniMe"><p> Loading...</p></div>
+                    <div className="aniMe"><p> Loading...</p></div>
+                    <div className="aniMe"><p> Loading...</p></div>
+                </div>
+                
+            }
             {blog && <BlogList posts={blog} header="My Day-to-Day Bible Read Verses" blogDelete={blogDelete}/>}
         </div>
     );
