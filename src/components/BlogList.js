36cdbbe1807844c311d53a19e9921838d46b78c6
yakubu-josiah <jossysmothest007@gmail.com>
2023-06-06 22:03:28 +0100

    fixing blog display outlining

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,10 +4,15 @@ const BlogList = ({posts, header, blogDelete}) => {
       <h1 className="pt-3">{header}</h1>
       {posts.map((post) => (
         <div className="preview-list" key={post.chapter}>
-          <h2> {post.book} </h2>
-          <p> This was written by: <span>{ post.writer }</span></p>
-          <button className="edit">read more &gt;&gt;</button>
-          <button className="del" onClick={() => blogDelete(post.chapter)}>delete &#x2715;</button>
+          <div>
+            <h2> {post.book} </h2>
+            <p> This was written by: <span>{ post.writer }</span></p>
+            {/* <p>{ post.verse }</p> */}
+          </div>
+          <div className="blogBtn">
+            <button className="edit">read more &gt;&gt;</button>
+            <button className="del" onClick={() => blogDelete(post.chapter)}>delete &#x2715;</button>
+          </div>
         </div>
       ))}   
     </div>
